14c2640caf33cd3ab32f04e58ca273a9db5dd44f
pavan-2001 <apavank71@gmail.com>
2021-03-27 00:06:27 +0530

    Connected to Firebase

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firsebase  from 'firebase';
+
+var firebaseConfig = {
+    apiKey: "AIzaSyBfkZMXfi2XK2O3yRiIQCy74hlPDnhGTzc",
+    authDomain: "find-my-course-ce248.firebaseapp.com",
+    projectId: "find-my-course-ce248",
+    storageBucket: "find-my-course-ce248.appspot.com",
+    messagingSenderId: "119588476937",
+    appId: "1:119588476937:web:8678ff7148808ab41380d8",
+    measurementId: "G-QLTSPW7BVK"
+  };
+  // Initialize Firebase
+  const fire=firebase.initializeApp(firebaseConfig);
+  export default fire;
